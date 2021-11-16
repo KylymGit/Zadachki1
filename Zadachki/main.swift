524755ec611132b93b0cf5fa5b1a4a3a232f2e8e
KylymGit <kylymgithub@gmail.com>
2021-11-16 16:30:29 +0600

    new

--- Zadachki/main.swift
+++ Zadachki/main.swift
@@ -5,7 +5,61 @@
 //  Created by mac on 11/11/21.
 //
 
-import Foundation
+//1) Напишите код который будет сортировать 2 числа по большему значению. (Используйте оператор if и больше меньше)
 
-print("Hello, World!")
+//print("Введите число:")
+//var number1 = readLine()!
+//var number2 = readLine()!
+//
+//if number1 > number2{
+//    print(number1)
+//    print(number2)
+//}else{
+//    print(number2)
+//    print(number1)
+//}
+
+//2) Напишите код который будет сортировать 2 текста по большему значению букв в тексте . (Используйте оператор if и больше меньше и count у текста)
+
+//print("Введите текст:")
+//var number1 = readLine()!
+//var number2 = readLine()!
+//
+//if number1.count > number2.count{
+//    print(number1)
+//    print(number2)
+//}else{
+//    print(number2)
+//    print(number1)
+//}
+
+
+
+
+////5) Напишите код который будет принимать в себе число (например 10 и другие числа введенные пользователем в терминале)  и одну букву (например “a” и другие буквы из терминала) и выводить текст с количеством символов (Используйте оператор for и 0…(3) range
+
+
+
+//print("Введите число:")
+//var number: Int = Int (readLine()!) ?? 0
+//
+//print("Введите букву:")
+//var letter = readLine()!
+//
+//for _ in 0...number - 1 {
+//    print(letter)
+//}
+
+
+//7) Напишите код который будет считать плошать прямоугольника по ширине и высоте чисел
+
+//print("Введите значение длины и ширины прямоугольника a и b")
+//
+//var a = readLine()!
+//
+//var b = readLine()!
+//
+//var result = Int(a)! * Int(b)!
+//print(result)
+
 
